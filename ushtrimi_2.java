4e0487b2a8b700da822b68bf7e8295d1d7a7dc4c
Aron Taci <arontacischool@gmail.com>
2026-04-05 13:34:12 +0200

    Permiresimet sipas feedbackut

--- ushtrimi_2.java
+++ ushtrimi_2.java
@@ -1,86 +1,98 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Scanner;
-class Figurat{
-    protected double b;
-    protected double h;
-    Figurat(double b,double h){
+
+abstract class Figura {
+    abstract double Llogarit_sip();
+    abstract String getType();
+}
+
+class Drejtkendeshi extends Figura {
+    private double b;
+    private double h;
+    Drejtkendeshi(double b, double h) {
         this.b = b;
         this.h = h;
-
     }
-    double Llogarit_sip(){
+    double Llogarit_sip() {
         return this.b * this.h;
     }
-   
-}
-class Drejtkendeshi extends Figurat{
-    Drejtkendeshi(double b, double h){
-        super(b,h);
+    String getType() {
+        return "Drejtkendeshi";
     }
 }
 
-class Trekendeshi extends Figurat{
-    Trekendeshi(double b, double h){
-        super(b,h);
+class Trekendeshi extends Figura {
+    private double b;
+    private double h;
+    Trekendeshi(double b, double h) {
+        this.b = b;
+        this.h = h;
     }
-    double Llogarit_sip(){
-        return (this.b * this.h)/2;
+    double Llogarit_sip() {
+        return (this.b * this.h) / 2;
+    }
+    String getType() {
+        return "Trekendeshi";
     }
 }
-class Rrethi{
-    double r;
-    Rrethi(double r){
+
+class Rrethi extends Figura {
+    private double r;
+    Rrethi(double r) {
         this.r = r;
     }
-    double Llogarit_sip(){
-        //Po e marr pie si 3.14 nese ska problem
-        return 3.14 * this.r * this.r;
+    double Llogarit_sip() {
+        return Math.PI * this.r * this.r;
+    }
+    String getType() {
+        return "Rrethi";
     }
 }
+
 public class ushtrimi_2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        while(true){
-        System.out.print("Zgjidhni 1 per rreth 2 per drejtkendesh dhe 3 per trekendesh kurse per te dale nga programi vendos -1");
-        int a = sc.nextInt();
-        if(a == -1)break;
-        switch(a){
-            case 1: {
-                System.out.print("Vendose rrezen: ");
-                double r = sc.nextDouble();
-                Rrethi rrethi = new Rrethi(r);
-                System.out.println("Siperfaqja e rrethit eshte: " + rrethi.Llogarit_sip());
-                break;
+        ArrayList<Figura> figures = new ArrayList<>();
+        while (true) {
+            System.out.print("Zgjidhni 1 per rreth 2 per drejtkendesh dhe 3 per trekendesh kurse per te dale nga programi vendos -1");
+            int a = sc.nextInt();
+            if (a == -1) break;
+            switch (a) {
+                case 1: {
+                    System.out.print("Vendose rrezen: ");
+                    double r = sc.nextDouble();
+                    Rrethi rrethi = new Rrethi(r);
+                    figures.add(rrethi);
+                    break;
+                }
+                case 2: {
+                    System.out.print("vendosni baze dhe lartesi: ");
+                    double b = sc.nextDouble();
+                    double h = sc.nextDouble();
+                    Drejtkendeshi drejtkendesh = new Drejtkendeshi(b, h);
+                    figures.add(drejtkendesh);
+                    break;
+                }
+                case 3: {
+                    System.out.print("Vendosi parametrat pra baze dhe lartesi: ");
+                    double b = sc.nextDouble();
+                    double h = sc.nextDouble();
+                    Trekendeshi trekendeshi = new Trekendeshi(b, h);
+                    figures.add(trekendeshi);
+                    break;
+                }
+                default:
+                    System.out.println("keni vendosur numrat e gabuar");
+                    break;
             }
-              case 2: {
-                System.out.print("vendosni baze dhe lartesi: ");
-                double b = sc.nextDouble();
-                double h = sc.nextDouble();
-                Drejtkendeshi drejtkendesh = new Drejtkendeshi(b,h);
-                System.out.println("Siperfaqja e drejtkendeshit eshte: " + drejtkendesh.Llogarit_sip());
-                break;
-
-            }
-
-            case 3:{
-                System.out.print("Vendosi parametrat pra baze dhe lartesi: ");
-                double b = sc.nextDouble();
-                double h = sc.nextDouble();
-                Trekendeshi trekendeshi = new Trekendeshi(b,h);
-                System.out.println("Trekendeshi ka nje siperfaqje prej: " + trekendeshi.Llogarit_sip());
-                break;
-            }
-            default: 
-            System.out.println("keni vendosur numrat e gabuar");
-            break;
-                
-            
-                
-
         }
+        // Sort by area
+        Collections.sort(figures, Comparator.comparingDouble(Figura::Llogarit_sip));
+        System.out.println("Figurat e renditura sipas siperfaqes:");
+        for (Figura f : figures) {
+            System.out.println(f.getType() + " ka siperfaqe: " + f.Llogarit_sip());
         }
-       
-        
-
-
-
-    }}
+    }
+}
